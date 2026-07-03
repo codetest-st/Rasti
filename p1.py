a558beba56ff2b27c2491d412222f47a31053ea3
codetest-st <codetest.st@gmail.com>
2026-07-03 16:42:37 +0530

    program1

--- p1.py
+++ p1.py
@@ -1,4 +1,4 @@
-n1=(input("enter first number:"))
-n2=(input("enter second number"))
+n1=int(input("enter first number:"))
+n2=int(input("enter second number"))
 total=n1-n2
 print(f"the subtraction is{total}")
