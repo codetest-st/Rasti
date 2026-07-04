8c4bb237e07107aa68eeec28db73e8ac4a15dddb
codetest-st <codetest.st@gmail.com>
2026-07-04 16:57:47 +0530

    program7

---
+++ rasti.py
@@ -0,0 +1,9 @@
+m=int(input("Enter marks of CSE:"))
+n=int(input("Enter marks of python:"))
+o=int(input("Enter marks of JAVA:"))
+p=int(input("Enter marks of C:"))
+q=int(input("Enter marks of HTML:"))
+a=m+n+o+p+q
+b= a/500*100
+print(f"The total of 5 subjects is {a}")
+print(f"The percentage of 5 subjects is{a}")
