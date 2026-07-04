3e469cfd07ffb74f6fdd86f5e82c57ca74d3c5a3
codetest-st <codetest.st@gmail.com>
2026-07-04 15:54:07 +0530

    program 2

---
+++ p2.py
@@ -0,0 +1,5 @@
+n1=int(input("enter first number"))
+n2=int(input("enter second number"))
+n3=int(input("enter third number"))
+a=n1*n2*n3
+print(f"the multiplication of {n1} ,{n2} and {n3} is {a}")
